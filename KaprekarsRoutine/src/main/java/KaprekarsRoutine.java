92cd6995cfd8322af142ccca4dcc6de28cacb9f6
DreamShine <wizard.jackalope@gmail.com>
2016-10-17 14:14:25 -0500

    Finished bonus #1 of writing a function that reverses a given four digit number.

--- KaprekarsRoutine/src/main/java/KaprekarsRoutine.java
+++ KaprekarsRoutine/src/main/java/KaprekarsRoutine.java
@@ -1,7 +1,16 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Created by DreamShine on 17/10/2016.
+ *
+ * Basic Challenge - From https://www.reddit.com/r/dailyprogrammer/comments/56tbds/20161010_challenge_287_easy_kaprekars_routine/
+ * Write a function that, given a 4-digit number,
+ * returns the largest digit in that number. Numbers between 0 and 999 are counted as 4-digit numbers with leading 0's.
+ *
+ * Bonus 1 -
+ * Write a function that, given a 4-digit number, performs the "descending digits" operation.
+ * This operation returns a number with the same 4 digits sorted in descending order.
  */
 public class KaprekarsRoutine
 {
@@ -28,5 +37,27 @@ public class KaprekarsRoutine
             if(number > largestNumber) largestNumber = number;
         }
         return largestNumber;
+    }
+
+    public static String descendingDigitsOf(int n) {
+        String stringRep = Integer.toString(n);
+
+
+        for(int lengthOfNumberString = stringRep.length();lengthOfNumberString - 4 != 0  ;lengthOfNumberString++)
+        {
+            stringRep = "0" + stringRep;
+        }
+        System.out.println(stringRep);
+       return reverseNumberString(stringRep);
+    }
+
+    private static String reverseNumberString(String stringRep)
+    {
+        String reversedNumberString = "";
+        for(int i = stringRep.length() - 1; i >= 0; i--)
+        {
+           reversedNumberString += stringRep.charAt(i);
+        }
+        return reversedNumberString;
     }
 }
--- KaprekarsRoutine/src/test/java/KaprekarsRoutineTest.java
+++ KaprekarsRoutine/src/test/java/KaprekarsRoutineTest.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import javax.security.auth.kerberos.KerberosPrincipal;
+import javax.swing.*;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
@@ -48,5 +51,35 @@ public class KaprekarsRoutineTest
     public void the_number_one_two_three_should_return_three_as_the_largest_number()
     {
         assertThat(KaprekarsRoutine.computeLargestNumber(123), is(3));
+    }
+
+    @Test
+    public void the_descending_order_of_0000_is_0000()
+    {
+        assertThat(KaprekarsRoutine.descendingDigitsOf(0000), is("0000"));
+    }
+
+    @Test
+    public void the_descending_order_of_1000_is_0001()
+    {
+        assertThat(KaprekarsRoutine.descendingDigitsOf(1000), is("0001"));
+    }
+
+    @Test
+    public void the_descending_order_of_2000_is_0002()
+    {
+        assertThat(KaprekarsRoutine.descendingDigitsOf(2000), is("0002"));
+    }
+
+    @Test
+    public void the_descending_order_of_1234_is_4321()
+    {
+        assertThat(KaprekarsRoutine.descendingDigitsOf(1234), is("4321"));
+    }
+
+    @Test
+    public void the_descending_order_of_21_is_1200()
+    {
+        assertThat(KaprekarsRoutine.descendingDigitsOf(21), is("1200"));
     }
 }
